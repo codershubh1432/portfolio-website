258180d9b5b76db968c08539a271d57e1d7f99df
codershubh1432 <167545010+codershubh1432@users.noreply.github.com>
2025-09-19 01:39:29 +0530

    Added website files

---
+++ script.js
@@ -0,0 +1,8 @@
+/* typing animation */
+
+var typed = new Typed(".typing",{
+  strings:[" ","Full Stack Developer", "Web developer", "Software Engineer"],
+  typespeed:50,
+  BackSpeed:30,
+  loop:true
+})

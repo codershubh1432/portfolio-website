801da01184e384c5cdf10c344811fadf79c1a792
codershubh1432 <167545010+codershubh1432@users.noreply.github.com>
2025-12-10 17:51:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ var typed = new Typed(".typing",{
   typespeed:50,
   BackSpeed:30,
   loop:true
+
 })
+
+const navToggler = document.querySelector('.nav-toggler');
+const aside = document.querySelector('.aside');
+const mainContent = document.querySelector('.main-content');
+
+navToggler.addEventListener('click', () => {
+  aside.classList.toggle('open');
+  mainContent.classList.toggle('shifted');
+});
